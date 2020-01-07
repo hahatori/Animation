f4974078e5f80233ab420a9de2beddbf3578b294
hahatori <56164812+hahatori@users.noreply.github.com>
2020-01-07 04:14:55 +0000

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot
 import agentframework
 import csv
 import numpy
-import matplotlib.animation # import
+import matplotlib.animation # import animation from matplotlib package.
 
 num_of_agents = 10      # Make a num_of_agents variable and assign it to 10
 num_of_iterations = 100 # Make a num_of_iterations variable and assign it to 100
@@ -14,7 +14,7 @@ environment = []        # Creat environment list.
 
 # Make animation properties.
 fig = matplotlib.pyplot.figure(figsize=(6, 6)) # Set up the figure and the plot size.
-ax = fig.add_axes([0, 0, 1, 1])  # Creat 
+ax = fig.add_axes([0, 0, 1, 1])  # Add axis list in the figure.
 
 #frames, = ax.plot(x, y)
 
@@ -44,14 +44,15 @@ for i in range(num_of_agents):
     
 #matplotlib.pyplot.show()
 
-carry_on = True
+# Start condition.
+carry_on = True  
 
 # Update data points.
-def update(frame_number):
-    fig.clear()   
+def update(frame_number): # Sets the number of animation frames
+    fig.clear()     # Clear a figure.
     global carry_on # carry_on is a global variable
     
-    # Move the agents.
+    # Move the agents by putting into nest for-loops.
     for j in range(num_of_iterations):
         for i in range(num_of_agents):
             agents[i].move("frame_number")
@@ -63,19 +64,22 @@ def update(frame_number):
         carry_on = False
         print("stopping condition")
     
+    # Set properties and show the animation plot.
     matplotlib.pyplot.xlim(0, 100)         # Set the x-axis range from 0 to 100.
     matplotlib.pyplot.ylim(0, 100)         # Set the y-axis range from 0 to 100.
     matplotlib.pyplot.imshow(environment)  # Display an image on the axes.
     matplotlib.pyplot.title(label = "Scatter Plot Animation") # Set plot title.
     
+    # Displays the random points obtained with the for-loop.
     for i in range(num_of_agents):
         matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
         matplotlib.pyplot.show()        
-     
+ 
+# Define a generator function.
 def gen_function(b = [0]):
     a = 0
     global carry_on # Display clearly, even if it is not assigned.
-    while (a < num_of_agents) & (carry_on) :
+    while (a < num_of_agents) & (carry_on) :  # While-loop.
         yield a			# Returns control and waits next call.
         a = a + 1
 
